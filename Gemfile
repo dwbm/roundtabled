19e50636fc9b184e6ff4f588a555930e285a9220
Daniel WB Mazzone <dwbm@danielwbmazzone.com>
2011-07-07 17:18:56 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3', '1.3.3'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'ruby-snarl'
+  gem 'test_notifier'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roundtabled::Application.config.secret_token = 'dec15f0307dcf1750159bbdd8ee80fec6451e16114d3519d3fd88ff85641677eaef25fe5ffce6908a4787589244f374dcbc7b085645245cf27cf5758a3378262'
